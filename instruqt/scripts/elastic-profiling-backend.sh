4e54d082298bbe1e20166381ca8ec671020023a9
Tyrone Bekiares <tyrone.bekiares@elastic.co>
2025-12-05 11:09:48 -0600

    ty

--- instruqt/scripts/elastic-profiling-backend.sh
+++ instruqt/scripts/elastic-profiling-backend.sh
@@ -6,36 +6,19 @@ export $(curl http://kubernetes-vm:9000/env | xargs)
 output=$(curl -s -X POST --header "Authorization: Basic $ELASTICSEARCH_AUTH_BASE64"  -H 'Content-Type: application/json' "$ELASTICSEARCH_URL/_security/api_key" -d '
 {
     "name": "profiling",
-    "expiration": "7d",
-    "role_descriptors": {
-        "profiling": {
-            "cluster": [
-                "monitor"
-            ],
-            "indices": [
-                {
-                    "names": [
-                        "profiling-*"
-                    ],
-                    "privileges": [
-                        "read",
-                        "write"
-                    ]
-                }
-            ]
-        }
-    }
+    "expiration": "7d"
 }
 ')
 export PROFILING_APIKEY=$(echo $output | jq -r '.encoded')
 
 cd /workspace/workshop
 
-envsubst < profiling/collector.yml > profiling/collector-rendered.yml
-envsubst < profiling/symbolizer.yml > profiling/symbolizer-rendered.yml
-
 kubectl patch Kibana kibana -p '{"spec": {"config": {"xpack.profiling.enabled": true}}}' --type=merge
 
-podman run -d --net host --name pf-elastic-collector -p 8260:8260 -v profiling/collector-rendered.yml:/pf-elastic-collector.yml:ro docker.elastic.co/observability/profiling-collector:9.2.2 -c /pf-elastic-collector.yml
+cd profiling
 
-podman run -d --net host --name pf-elastic-symbolizer -p 8240:8240 -v profiling/symbolizer-rendered.yml:/pf-elastic-symbolizer.yml:ro docker.elastic.co/observability/profiling-symbolizer:9.2.2 -c /pf-elastic-symbolizer.yml
+envsubst < collector.yml > collector-rendered.yml
+envsubst < symbolizer.yml > symbolizer-rendered.yml
+
+podman run -d --net host --name pf-elastic-collector -p 8260:8260 -v $PWD/collector-rendered.yml:/pf-elastic-collector.yml:ro --rm docker.elastic.co/observability/profiling-collector:9.2.2 -c /pf-elastic-collector.yml
+podman run -d --net host --name pf-elastic-symbolizer -p 8240:8240 -v $PWD/symbolizer-rendered.yml:/pf-elastic-symbolizer.yml:ro --rm docker.elastic.co/observability/profiling-symbolizer:9.2.2 -c /pf-elastic-symbolizer.yml
