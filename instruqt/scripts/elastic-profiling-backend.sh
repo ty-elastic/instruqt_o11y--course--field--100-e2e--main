ed12e5bb6c5ae4f1ee7b885be191efe191d06e4c
Tyrone Bekiares <tyrone.bekiares@elastic.co>
2025-12-06 07:58:24 -0600

    ty

--- instruqt/scripts/elastic-profiling-backend.sh
+++ instruqt/scripts/elastic-profiling-backend.sh
@@ -6,24 +6,24 @@ export $(curl http://kubernetes-vm:9000/env | xargs)
 kubectl patch Kibana kibana -p '{"spec": {"config": {"xpack.profiling.enabled": true}}}' --type=merge
 retry_command_lin check_es_health
 
-# echo "/api/profiling/setup/es_resources"
-# setup_profiling() {
-#     local http_status=$(curl -s -X POST "$KIBANA_URL/api/profiling/setup/es_resources" \
-#       --header 'Content-Type: application/json' \
-#       --header 'x-elastic-internal-origin: Kibana' \
-#       --header "Authorization: Basic $ELASTICSEARCH_AUTH_BASE64" \
-#       --header 'kbn-xsrf: true'
-#     )
+echo "/api/profiling/setup/es_resources"
+setup_profiling() {
+    local http_status=$(curl -s -X POST "$KIBANA_URL/api/profiling/setup/es_resources" \
+      --header 'Content-Type: application/json' \
+      --header 'x-elastic-internal-origin: Kibana' \
+      --header "Authorization: Basic $ELASTICSEARCH_AUTH_BASE64" \
+      --header 'kbn-xsrf: true'
+    )
 
-#     if echo $http_status | grep -q '^2'; then
-#         echo "Profiling successfully initialized: $http_status"
-#         return 0
-#     else
-#         echo "Failed to initialize Profiling. HTTP status: $http_status"
-#         return 1
-#     fi
-# }
-# retry_command_lin setup_profiling
+    if echo $http_status | grep -q '^2'; then
+        echo "Profiling successfully initialized: $http_status"
+        return 0
+    else
+        echo "Failed to initialize Profiling. HTTP status: $http_status"
+        return 1
+    fi
+}
+retry_command_lin setup_profiling
 
 # ------------- SERVICES
 
