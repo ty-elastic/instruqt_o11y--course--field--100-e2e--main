6a0452d689b19199b6a6132d6047074d6f321b21
Tyrone Bekiares <tyrone.bekiares@elastic.co>
2026-01-29 20:24:30 -0600

    ty

--- instruqt/scripts/vars.sh
+++ instruqt/scripts/vars.sh
@@ -1,9 +1,9 @@
 source /workspace/workshop/instruqt/scripts/retry.sh
 
-if ! host es3-api > /dev/null; then
-    retry_command_lin curl http://kubernetes-vm:9000/env
-    export $(curl http://kubernetes-vm:9000/env | xargs)
-else
+if [ -n "$(dig +short es3-api)" ]; then
     retry_command_lin curl http://es3-api:9000/env
     export $(curl http://es3-api:9000/env | xargs)
+else
+    retry_command_lin curl http://kubernetes-vm:9000/env
+    export $(curl http://kubernetes-vm:9000/env | xargs)
 fi
