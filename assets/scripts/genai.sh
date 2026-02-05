db0f49929d2fcf5d2a92620180839af3640200a4
Tyrone Bekiares <tyrone.bekiares@elastic.co>
2026-02-04 19:52:49 -0600

    ty

--- assets/scripts/genai.sh
+++ assets/scripts/genai.sh
@@ -20,5 +20,16 @@ config_genai() {
       -H "Authorization: ApiKey ${elasticsearch_api_key}" \
       -H 'x-elastic-internal-origin: Kibana' \
       -d '{"changes":{"aiAssistant:preferredChatExperience": "agent", "genAiSettings:defaultAIConnector": "Anthropic-Claude-Sonnet-4-5"}}'
+
+  curl -X POST "$elasticsearch_kibana_endpoint/internal/product_doc_base/install" \
+      -H 'Content-Type: application/json' \
+      -H 'kbn-xsrf: true' \
+      -H "Authorization: ApiKey ${elasticsearch_api_key}" \
+      -H 'x-elastic-internal-origin: Kibana' \
+      -d '{"inferenceId":".elser-2-elasticsearch","resourceType":"product_doc"}'
 }
 config_genai
+
+
+
+
