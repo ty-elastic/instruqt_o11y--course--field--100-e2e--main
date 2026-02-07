a5daae9331fbaefab60af9c2f95d4302523a836f
Tyrone Bekiares <tyrone.bekiares@elastic.co>
2026-02-07 12:42:53 -0600

    ty

--- instruqt/scripts/vars.sh
+++ instruqt/scripts/vars.sh
@@ -25,9 +25,9 @@ done
 
 if [ "$wait" = "true" ]; then
     if [[ "$HOSTNAME" == "es3-api" ]]; then
-        retry_command_lin curl http://es3-api:9000/env
+        retry_command_lin curl -s http://es3-api:9000/env
     elif [[ "$HOSTNAME" == "k3s" ]]; then
-        retry_command_lin curl http://es3-api:9000/env
+        retry_command_lin curl -s http://es3-api:9000/env
     fi
 fi
 
