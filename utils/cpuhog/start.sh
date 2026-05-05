569dafbf5772e7af8b98ce344a1c5b8bf7b10888
Tyrone Bekiares <tyrone.bekiares@elastic.co>
2026-05-05 16:44:42 -0500

    ty

--- utils/cpuhog/start.sh
+++ utils/cpuhog/start.sh
@@ -1,11 +1,6 @@
-source /opt/workshops/elastic-retry.sh
-export $(curl http://kubernetes-vm:9000/env | xargs)
-
-export INSTRUQT_TRACK_SLUG=o11y--course--field--100-e2e--main
+export INSTRUQT_TRACK_SLUG=o11y--course--field--100-e2e--serverless
 export REPO=us-central1-docker.pkg.dev/elastic-sa/tbekiares
 export COURSE=$INSTRUQT_TRACK_SLUG
 
-cd /workspace/workshop
-export NAMESPACE=trading-na
-envsubst < cpuhog/cpuhog.yaml | kubectl apply -f -
-cd /workspace/
+export NAMESPACE=utils
+envsubst < cpuhog.yaml | kubectl apply -f -
