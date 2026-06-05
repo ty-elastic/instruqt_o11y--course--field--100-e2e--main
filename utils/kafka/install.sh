b0a75dd5bb4e0ee97e340089b552d8ff7b402889
Tyrone Bekiares <tyrone.bekiares@elastic.co>
2026-06-05 13:11:39 -0500

    ty

--- utils/kafka/install.sh
+++ utils/kafka/install.sh
@@ -13,8 +13,8 @@ echo $values
 
 kubectl create namespace infra
 
-# kubectl create -f 'https://strimzi.io/install/latest?namespace=infra' -n infra
-# kubectl wait --for=condition=Ready pod --all -n infra --timeout=300s
+kubectl create -f 'https://strimzi.io/install/latest?namespace=infra' -n infra
+kubectl wait --for=condition=Ready pod --all -n infra --timeout=300s
 
 kubectl apply -f $values -n infra 
 kubectl wait kafka/my-cluster --for=condition=Ready --timeout=300s -n infra
