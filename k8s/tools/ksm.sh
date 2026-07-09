8e52af406f1fcdc5dbe56443b9e2d13bdaebf428
Ty Bekiares <tyrone.bekiares@elastic.co>
2026-07-09 15:53:47 -0500

    ty

--- k8s/tools/ksm.sh
+++ k8s/tools/ksm.sh
@@ -1,3 +1,7 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install --set namespaceOverride=kube-system kube-state-metrics prometheus-community/kube-state-metrics
+if kubectl get deployments --all-namespaces -l app.kubernetes.io/name=kube-state-metrics | grep -q "kube-state-metrics"; then
+    echo "kube-state-metrics is already installed. Skipping installation."
+else
+    helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+    helm repo update
+    helm install --set namespaceOverride=kube-system kube-state-metrics prometheus-community/kube-state-metrics
+fi
