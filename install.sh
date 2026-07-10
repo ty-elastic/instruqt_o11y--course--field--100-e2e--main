62292fcf51460412c09829d9e0698d38fb8a767d
Ty Bekiares <tyrone.bekiares@elastic.co>
2026-07-09 19:04:56 -0500

    ty

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ fi
 ./build.sh -t $FLEET_URL -c $COURSE -1 true -j $ELASTICSEARCH_URL -h $KIBANA_URL -i $ELASTICSEARCH_APIKEY -k $INGEST_URL
 
 # services
-./build.sh -o serverless -t $FLEET_URL -c $COURSE -d true -n true -b false -s all -j $ELASTICSEARCH_URL -h $KIBANA_URL -i $ELASTICSEARCH_APIKEY -k $INGEST_URL
+./build.sh -o serverless -t $FLEET_URL -c $COURSE -d true -n false -b false -s all -j $ELASTICSEARCH_URL -h $KIBANA_URL -i $ELASTICSEARCH_APIKEY -k $INGEST_URL
 
 # synthetics, profiling
 ./build.sh -o false -t $FLEET_URL -c $COURSE -d false -b false -s none -j $ELASTICSEARCH_URL -h $KIBANA_URL -i $ELASTICSEARCH_APIKEY -k $INGEST_URL -p true -m true
